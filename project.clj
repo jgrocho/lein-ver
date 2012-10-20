b39771ed5bc209dd08ff26d57fa098f5a439ec0d
Jonathan Grochowski <jongrocho@gmail.com>
2012-10-20 00:28:18 -0700

    Start dogfooding it

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-ver "0.0.0"
+(defproject lein-ver "0.1.0"
   :description "A lein plugin for handling a project's version"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
