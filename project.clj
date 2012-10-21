2b97a9e8301b972fab743bb4168301729be94bc8
Jonathan Grochowski <jongrocho@gmail.com>
2012-10-20 17:19:54 -0700

    Set project url

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject lein-ver "0.1.0"
   :description "A lein plugin for handling a project's version"
+  :url "https://github.com/jgrocho/lein-ver"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
