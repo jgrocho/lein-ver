bcad5e01ac86ee21b2479eec88ba46c3b12d61dd
Aaron France <aaron.l.france@gmail.com>
2014-02-18 12:51:46 +0100

    Don't use file on the resource.
    If the resource is in a jar file it will not exist as a file and thus
    will throw. The behaviour changed here will mean that `lein uberjar's
    built using lein-ver will function correctly.

--- src/lein_ver/version.clj
+++ src/lein_ver/version.clj
@@ -2,16 +2,15 @@
   (:require [clojure.java.io :refer [file reader resource]]
             [clojure.string :refer [join]])
   (:import java.io.PushbackReader))
-(let [@version-file (file (resource "VERSION"))]
-  (when (.exists version-file)
-    (with-open [@rdr (reader version-file)]
-      (binding [*read-eval* false]
-        (def version (read (PushbackReader. rdr)))
-        (def major (:major version))
-        (def minor (:minor version))
-        (def patch (:patch version))
-        (def pre-release (:pre-release version))
-        (def build (:build version))
-        (def string (str (join "." (filter identity [major minor patch]))
-                         (when pre-release (str "-" pre-release))
-                         (when build (str "+" build)))))))))
+(when-let [@version-file (resource "VERSION")]
+  (with-open [@rdr (reader version-file)]
+    (binding [*read-eval* false]
+      (def version (read (PushbackReader. rdr)))
+      (def major (:major version))
+      (def minor (:minor version))
+      (def patch (:patch version))
+      (def pre-release (:pre-release version))
+      (def build (:build version))
+      (def string (str (join "." (filter identity [major minor patch]))
+                       (when pre-release (str "-" pre-release))
+                       (when build (str "+" build)))))))
